283de693787963e323154fffdf6c88548989c8f3
tranhanh18042 <hanhtran18042@gmial.com>
2023-04-12 11:32:07 +0700

    change password

---
+++ app/Http/Controllers/ChangePasswordController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ChangePassword;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class ChangePasswordController extends Controller
+{
+    public function index(){
+        return view('changePassword');
+    }
+    public function changePassword(ChangePassword $request){
+        $input = $request->safe()->only(['current_password', 'new_password','confirm_password']);
+        $user = Auth::user();
+        $checkPass = Hash::check($input['current_password'],$user->password);
+        if($checkPass == true){
+            if($input['current_password'] != $input['new_password'] && $input['new_password'] == $input['confirm_password']){
+                User::whereId(auth()->user()->id)->update([
+                    'password' => Hash::make($input['new_password'])
+                ]);
+                Auth::logout();
+
+                return redirect()->route('login');
+            }
+        }else if($input != null){
+            Session::flash('error',"nhap sai thong tin");
+            return redirect()->back();
+        }
+        Session::flash('error',"Can nhap du thong tin");
+        return redirect()->back();
+    }
+}
---
+++ resources/views/changePassword.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
+
+        <title>Hello, world!</title>
+      </head>
+<body>
+
+    <section>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <!-- Container wrapper -->
+          <div class="container-fluid">
+            <!-- Toggle button -->
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-mdb-toggle="collapse"
+              data-mdb-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <i class="fas fa-bars"></i>
+            </button>
+        
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <!-- Navbar brand -->
+              <a class="navbar-brand mt-2 mt-lg-0" href="#">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+                  height="15"
+                  alt="MDB Logo"
+                  loading="lazy"
+                />
+              </a>
+              <!-- Left links -->
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" href="{{route('home')}}">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Subject</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{route('profile')}}">Profile</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{route('login')}}">Logout</a>
+                </li>
+              </ul>
+              <!-- Left links -->
+            </div>
+            <!-- Collapsible wrapper -->
+        
+            <!-- Right elements -->
+            <div class="d-flex align-items-center">
+              <!-- Icon -->
+              <a class="text-reset me-3" href="#">
+                <i class="fas fa-shopping-cart"></i>
+              </a>
+            <!-- Right elements -->
+          </div>
+          <!-- Container wrapper -->
+        </nav>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- Page title -->
+                <div class="my-5">
+                    <h3>Change Password</h3>
+                    <hr>
+                </div>
+                <!-- Form START -->
+                <form action="" method="POST">
+                  @csrf
+                  @method("PUT")
+                  @include('alert')
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Current Password</label>
+                    <input name="current_password" type="password" class="form-control" id="exampleInputPassword1" aria-describedby="emailHelp">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword2" class="form-label">New Password</label>
+                    <input name="new_password" type="password" class="form-control" id="exampleInputPassword2">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword3" class="form-label">Confirm Password</label>
+                    <input name="confirm_password" type="password" class="form-control" id="exampleInputPassword3">                  
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+              </div>
+        </div>
+    </div>
+</body>
