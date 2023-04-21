5e41ec48bd8930b0d2192ab0507193a94b3c1059
tranhanh18042 <hanhtran18042@gmial.com>
2023-04-21 10:47:48 +0700

    function reset password

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
